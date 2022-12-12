788e4e204f7f43cf818a5ab8725bd994014e5a25
gibbs2b <guilhermegoldnerdasilva@gmail.com>
2022-12-12 20:37:12 -0300

    Improve supplier search

--- Gibbs2b.DtoGenerator.Design/DbContextExtensions.cs
+++ Gibbs2b.DtoGenerator.Design/DbContextExtensions.cs
@@ -1,9 +1,53 @@
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
+using NpgsqlTypes;
 
 namespace Gibbs2b.DtoGenerator;
 
 public static class DbContextExtensions
 {
+    public static void HasTsVector<TEntity>(this ModelBuilder modelBuilder,
+        Expression<Func<TEntity, NpgsqlTsVector>> field,
+        Expression<Func<TEntity, object>> include,
+        Expression<Func<TEntity, object?>> index,
+        string config = "english") where TEntity : class
+    {
+        modelBuilder
+            .Entity<TEntity>()
+            .HasGeneratedTsVectorColumn(
+                field,
+                config,
+                include)
+            .HasIndex(index)
+            .HasMethod("GIN");
+    }
+
+    public static void HasTsVector<TEntity>(this ModelBuilder modelBuilder,
+        Expression<Func<TEntity, NpgsqlTsVector>> field,
+        string config = "english") where TEntity : class
+    {
+        var lambda = field as LambdaExpression;
+
+        modelBuilder.HasTsVector(
+            field,
+            Expression.Lambda<Func<TEntity, object>>(lambda.Body, false, lambda.Parameters),
+            config);
+    }
+
+    public static void HasTsVector<TEntity>(this ModelBuilder modelBuilder,
+        Expression<Func<TEntity, NpgsqlTsVector>> field,
+        Expression<Func<TEntity, object>> include,
+        string config = "english") where TEntity : class
+    {
+        var lambda = field as LambdaExpression;
+
+        modelBuilder.HasTsVector(
+            field,
+            include,
+            Expression.Lambda<Func<TEntity, object?>>(lambda.Body, false, lambda.Parameters),
+            config);
+    }
+
     public static void ExcludeView<TModel>(this ModelBuilder builder, string tableName) where TModel : class
     {
         builder
