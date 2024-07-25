049cd3cb9266399aaa7aa0b7920ca8eab8fda252
gibbs2b <guilhermegoldnerdasilva@gmail.com>
2024-07-25 20:34:16 -0300

    Add enum absolute path option

--- Gibbs2b.DtoGenerator.Design.Spec/Model/TypescriptProjectSpec.cs
+++ Gibbs2b.DtoGenerator.Design.Spec/Model/TypescriptProjectSpec.cs
@@ -28,6 +28,7 @@ public class TypescriptProjectSpec
 
     public string DefaultDtoPath { get; set; } = null!;
     public string? DefaultEnumPath { get; set; }
+    public string? EnumPathPrefix { get; set; }
     public string DefaultHandlerPath { get; set; } = null!;
     public bool IsFetch { get; set; }
     public bool Disabled { get; set; }
