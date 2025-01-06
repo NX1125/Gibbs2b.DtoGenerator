c7934298837a0a08ca856f64793b32799ece7764
gibbs2b <guilhermegoldnerdasilva@gmail.com>
2025-01-06 09:55:12 -0300

    Deprecate TsOptional

--- Gibbs2b.DtoGenerator.Design/Annotation/TsOptionalAttribute.cs
+++ Gibbs2b.DtoGenerator.Design/Annotation/TsOptionalAttribute.cs
@@ -1,9 +1,12 @@
+using Gibbs2b.DTO.Generator.Typescript.Annotation;
+
 namespace Gibbs2b.DtoGenerator.Annotation;
 
 using System;
 
+[Obsolete]
 [AttributeUsage(AttributeTargets.Property)]
-public class TsOptionalAttribute : Attribute
+public class TsOptionalAttribute : DtoOptionalAttribute
 {
     /// <summary>
     /// Whether <code>null</code> and <code>undefined</code> should be allowed.
